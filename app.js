fb1b9bcac916dbddd30a69642ce8d109fd553a9f
VicKono <viktor.konopelko@heku-it.com>
2020-08-14 15:19:46 +0300

    Added functionality and corrected the markup

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 "use strict";
+
+let clicks = 1;
+
+const TIMEOUT = 5000,
+    btn = document.getElementById('clicker'),
+    title = document.getElementById('title');
+    timer = document.getElementById('timer'),
+    score = document.getElementById('score');
+
+btn.onclick = start;
+
+function start() {
+    const startTime = Date.now();
+
+    timer.textContent = formatTime(TIMEOUT);
+    btn.onclick = () => score.textContent = clicks++;
+
+    const interval = setInterval(() => {
+        const delta = Date.now() - startTime;
+        timer.textContent = formatTime(TIMEOUT - delta);
+    }, 100);
+
+    const timeout = setTimeout(() => {
+        btn.onclick = null;
+        timer.textContent = 'Game Over';
+
+        clearInterval(interval);
+        clearTimeout(timeout);
+    }, TIMEOUT);
+}
+
+function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
+}
+
